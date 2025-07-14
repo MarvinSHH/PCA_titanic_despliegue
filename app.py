2cb351261f82b2603fd193c98f57c2459340c6a0
Marvin SH <mardevsh@gmail.com>
2025-07-14 13:37:40 -0600

    PCA titanic

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__)  # ← ¡Esto debe ir antes de cualquier @app.route!
+model = joblib.load("modelrf.pkl")  # Asegúrate de que model.pkl esté en la misma carpeta
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        feats = np.array(data["features"], dtype=float).reshape(1, -1)
+        pred = model.predict(feats)[0]
+        return jsonify({"prediction": float(pred)})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
