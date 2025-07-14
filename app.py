1f74d4518742016d55fa20b08684db52f1a1a39b
Marvin SH <20221135@uthh.edu.mx>
2025-07-14 14:29:28 -0600

    modificado, listo y funcional

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 
-app = Flask(__name__)  # ← ¡Esto debe ir antes de cualquier @app.route!
-model = joblib.load("modelrf.pkl")  # Asegúrate de que model.pkl esté en la misma carpeta
+app = Flask(__name__)
+model = joblib.load("model_svm_pca.pkl")
 
 @app.route("/")
 def home():
@@ -13,11 +13,11 @@ def home():
 def predict():
     try:
         data = request.get_json()
-        feats = np.array(data["features"], dtype=float).reshape(1, -1)
-        pred = model.predict(feats)[0]
-        return jsonify({"prediction": float(pred)})
+        features = np.array(data["features"]).reshape(1, -1)
+        prediction = model.predict(features)[0]
+        return jsonify({"prediction": int(prediction)})
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
